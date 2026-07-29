52286d9c6ed289266dd4d835de0ae1ba68c7172d
marzouq-roboticscenterAI <marzouq@roboticscenter.ai>
2026-07-28 20:00:42 -0700

    build: reproduce the full pinned OpenArm source set

--- scripts/fetch_upstreams.sh
+++ scripts/fetch_upstreams.sh
@@ -28,3 +28,17 @@ fetch_pinned openarm_can https://github.com/enactic/openarm_can.git \
   c32ecd31da267967f0c913c2118c843177d88b91
 fetch_pinned openarm_ros2 https://github.com/enactic/openarm_ros2.git \
   4e837e1d0dae692ff67b560b69d8d281d7a8d4ed
+fetch_pinned openarm https://github.com/enactic/openarm.git \
+  990fda921c82ae9d12b00f23e449793a9a313afd
+fetch_pinned openarm_hardware https://github.com/enactic/openarm_hardware.git \
+  12c07510c09b2c10b7dfe48010dae5c05cbe887f
+fetch_pinned openarm_teleop https://github.com/enactic/openarm_teleop.git \
+  eb2d49338bf70ace95282ea724903849397b7811
+fetch_pinned openarm_isaac_lab https://github.com/enactic/openarm_isaac_lab.git \
+  bad82e23716e6941c2de78ccb978f57c78b37734
+fetch_pinned openarm_mujoco https://github.com/enactic/openarm_mujoco.git \
+  8955afb54e4adfb59a236e2b4d15192b7a02865c
+fetch_pinned openarm_dataset https://github.com/enactic/openarm_dataset.git \
+  2da1062f524a5b240e61e1031f637d765076569d
+fetch_pinned dora-openarm https://github.com/enactic/dora-openarm.git \
+  d988dd24537d670f07b6d6e85e0cdd25f0b05b82
